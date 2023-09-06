b68b67bcd46be2f84bb71f313c581650da8a280f
James Fisher <mrwhitepantz@hotmail.com>
2023-09-06 07:56:40 -0700

    reading stdin and creating matrix complete

--- determinant_calculator/src/main.rs
+++ determinant_calculator/src/main.rs
@@ -1,25 +1,32 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-	// take stdin to lines iterator
-	let mut lines = io::stdin().lines();
-	let n = lines.next().unwrap()?.trim().parse::<f64>().unwrap() as usize;
+	let stdin = io::stdin();
+	let mut buffer = String::new();
+	stdin.read_line(&mut buffer).unwrap();
+	let n = buffer.trim().parse::<usize>().unwrap();
+	buffer.clear();
 
 	// Build n x n matrix from stdin lines
-	let mut matrix_rows: Vec<Vec<f64>> = Vec::with_capacity(n);
-	while let Some(line) = lines.next() {
-		let mut matrix_cols:Vec<f64> = Vec::with_capacity(n);
-		while let Some(num) = line?.split_whitespace().next() {
-			matrix_cols.push(num.parse::<f64>().unwrap());
-		}
-		matrix_rows.push(matrix_cols);
+	let mut _matrix_rows: Vec<Vec<f64>> = Vec::with_capacity(n);
+	let mut row: usize = 0;
+	
+	while row < n {
+		let line = stdin.read_line(&mut buffer);
+		let mut _matrix_cols: Vec<f64> = buffer.split_whitespace().map(|s| s.parse::<f64>().unwrap()).collect();
+		buffer.clear();
+		_matrix_rows.push(_matrix_cols);
+		row += 1;
 	}
 
 	let mut determinant: f64 = 1.0;
-	for (i,ele) in matrix_rows.iter().enumerate() {
-		determinant *= ele[i];
+
+
+	for (col,row) in _matrix_rows.iter().enumerate() {
+		determinant *= row[col];
 	}
 	
+	println!("{determinant}");
 	return Ok(());
 
 }
---
+++ determinant_calculator/target/debug/deps/determinant_calculator-75c0fa347db58422.d
@@ -0,0 +1,5 @@
+D:\School Supplies\WSU\2023 Fall\CS450-MachineLearning\Project1\determinant_calculator\target\debug\deps\determinant_calculator-75c0fa347db58422.exe: src\main.rs
+
+D:\School Supplies\WSU\2023 Fall\CS450-MachineLearning\Project1\determinant_calculator\target\debug\deps\determinant_calculator-75c0fa347db58422.d: src\main.rs
+
+src\main.rs:

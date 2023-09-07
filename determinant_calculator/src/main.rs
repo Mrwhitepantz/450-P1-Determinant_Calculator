e56086878967c15f1f9c1495884ae074f7728811
James Fisher <mrwhitepantz@hotmail.com>
2023-09-07 10:03:06 -0700

    first attempts at triangularization (not working)

--- determinant_calculator/src/main.rs
+++ determinant_calculator/src/main.rs
@@ -11,19 +11,79 @@ fn main() -> io::Result<()> {
 	buffer.clear();
 
 	// build n x n matrix from rows provided through stdin
-	let mut _matrix_rows: Vec<Vec<f64>> = Vec::with_capacity(n);
+	let mut matrix_rows: Vec<Vec<f64>> = Vec::with_capacity(n);
 
 	for _ in 0..n {
-		let line = stdin.read_line(&mut buffer);
-		let mut _matrix_cols: Vec<f64> = buffer.split_whitespace().map(|s| s.parse::<f64>().unwrap()).collect();
+		let _ = stdin.read_line(&mut buffer);
+		let matrix_cols: Vec<f64> = buffer.split_whitespace().map(|s| s.parse::<f64>().unwrap()).collect();
 		buffer.clear();
-		_matrix_rows.push(_matrix_cols);
+		matrix_rows.push(matrix_cols);
 	}
-
+	
 	let mut determinant: f64 = 1.0;
 
+	println!("Read Matrix:");
+	for row in &matrix_rows {
+		print!("[ ");
+		for col in row {
+			print!("{col} ");
+		}
+		println!("]");
+	}
 
-	for (col,row) in _matrix_rows.iter().enumerate() {
+	println!("-----------------");
+
+	println!("Starting Triangularization");
+	
+	for i in 0..n {
+		println!("Column: {i}");
+		let mut max: f64 = 0.0;
+		let mut max_index = 0;
+		for (index,row) in matrix_rows.iter_mut().enumerate() {
+			if index >= i{
+				println!("Checking for max at [{index}, {i}]");
+				if row[i] > max {
+					println!("New max found");
+					max = row[i];
+					max_index = index
+				}
+			}	
+		}
+		println!("Column max index: {max_index} needs to be in row {i}");
+		if max_index != i {
+			println!("swapping row {max_index} with row {i}");
+			matrix_rows.swap(i,max_index);
+			determinant *= -1.0;
+		}
+		println!("");
+		println!("Starting Scaling");
+		for (index, row) in matrix_rows.iter_mut().enumerate() {
+			println!("Index: {index} i: {i}");
+		    if index > i {
+				println!("index greater than i");
+				let scale_factor = row[i]/ *(&mut matrix_rows[i][i]);
+				println!("scale factor: {} / {} = {}", row[i], &mut matrix_rows[i][i],scale_factor);
+				for (j, mut col) in row.iter().enumerate() {
+					println!("changing [{index}, {j}]");
+					println!("should be {col} - {scale_factor}*{}",&mut matrix_rows[i][j]);
+					matrix_rows[index][j] = col - scale_factor* (*(&mut matrix_rows[i][j]));
+					println!("now is: {}",&mut matrix_rows[index][j]);
+				}
+			}
+		}
+
+		println!("New Matrix:");
+		for row in &matrix_rows {
+			print!("[ ");
+			for col in row {
+				print!("{col} ");
+			}
+			println!("]");
+		}
+	}
+
+	for (col,row) in matrix_rows.iter().enumerate() {
+		println!("{col}:{}",row[col]);
 		determinant *= row[col];
 	}
 	

ab343b9fad15dca44426352cf8108419b860a297
James Fisher <mrwhitepantz@hotmail.com>
2023-09-07 19:44:23 -0700

    Complete

--- determinant_calculator/src/main.rs
+++ determinant_calculator/src/main.rs
@@ -1,27 +1,24 @@
 use std::io;
-
+use std::io::BufRead;
 fn main() -> io::Result<()> {
 	// set up stdin for reading
 	let stdin = io::stdin();
+	let mut stdin = stdin.lock();
 	let mut buffer = String::new();
-	
 	// read matrix dimension and clear buffer
-	stdin.read_line(&mut buffer).unwrap();
+	let _ = stdin.read_line(&mut buffer);
 	let n = buffer.trim().parse::<usize>().unwrap();
 	buffer.clear();
-
 	// build n x n matrix from rows provided through stdin
 	let mut matrix_rows: Vec<Vec<f64>> = Vec::with_capacity(n);
-
 	for _ in 0..n {
 		let _ = stdin.read_line(&mut buffer);
 		let matrix_cols: Vec<f64> = buffer.split_whitespace().map(|s| s.parse::<f64>().unwrap()).collect();
 		buffer.clear();
 		matrix_rows.push(matrix_cols);
 	}
-	
 	let mut determinant: f64 = 1.0;
-
+	// make the matrix upper triangular and find determinant
 	for col_index in 0..n {
 		let mut max_val:  f64 = 0.0;
 		let mut max_index = 0;
@@ -44,12 +41,9 @@ fn main() -> io::Result<()> {
 			}
 		}
 	}
-
-
 	for i in 0..n {
 		determinant *= matrix_rows[i][i];
 	}
-	
 	println!("{determinant}");
 	return Ok(());
 
